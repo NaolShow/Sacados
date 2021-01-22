bb82f27df3eefa247a231d7bc373516f90197e4f
NaolShow <naolyt@gmail.com>
2021-01-22 18:44:07 +0100

    Item and ItemStack

---
+++ Assets/Sacados/Core/Items/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Sacados.Core.Items {
+
+#if UNITY_EDITOR
+    [CreateAssetMenu(fileName = "New Item", menuName = "Sacados/Item")]
+#endif
+    public class Item : ScriptableObject {
+
+        /// <summary>
+        /// ID of the Item
+        /// </summary>
+        public string ID;
+
+        /// <summary>
+        /// Max stack size of the Item
+        /// </summary>
+        public int MaxStackSize;
+
+    }
+
+}
---
+++ Assets/Sacados/Core/Items/ItemStack.cs
@@ -0,0 +1,47 @@
+﻿namespace Sacados.Core.Items {
+
+    public class ItemStack {
+
+        #region Is Properties
+
+        /// <summary>
+        /// Determines if the ItemStack is empty (it's Item is null or it's StackSize <= 0)
+        /// </summary>
+        public bool IsEmpty => (Item == null) || (StackSize <= 0);
+
+        #endregion
+
+        /// <summary>
+        /// Item of the ItemStack
+        /// </summary>
+        public Item Item;
+
+        /// <summary>
+        /// Stack size of the ItemStack
+        /// </summary>
+        public int StackSize;
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates an ItemStack with the specified Item and StackSize
+        /// </summary>
+        public ItemStack(Item item, int stackSize) {
+
+            // Save the Item and the StackSize
+            Item = item;
+            StackSize = stackSize;
+
+        }
+
+        /// <summary>
+        /// Creates an ItemStack with the specified Item and it's MaxStackSize as the StackSize
+        /// </summary>
+        /// <param name="item"></param>
+        public ItemStack(Item item) : this(item, item.MaxStackSize) { }
+
+        #endregion
+
+    }
+
+}
