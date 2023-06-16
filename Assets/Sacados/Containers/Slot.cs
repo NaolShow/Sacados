5397e313e3bb0e77299d3cb070d8dba8e3d2145a
NaolShow <loan.jouhannet@protonmail.com>
2023-06-17 00:27:08 +0200

    feat(slot): allow overriding base ItemStack property

--- Assets/Sacados/Containers/Slot.cs
+++ Assets/Sacados/Containers/Slot.cs
@@ -11,7 +11,7 @@ namespace Sacados {
 
         }
 
-        public virtual new T ItemStack { get => (T)base.ItemStack; set => base.ItemStack = value; }
+        public virtual new T ItemStack { get => (T)base.Get(); set => base.Set(value); }
 
         // User implementation
         public virtual uint GetMaxStackSize(T itemStack) => base.GetMaxStackSize(itemStack);
@@ -24,22 +24,30 @@ namespace Sacados {
         public virtual bool CanBeTaken(T itemStack) => true;
 
         // Converts the ItemStack to T and call the new methods
+        protected sealed override ItemStack Get() => base.Get();
+        protected sealed override void Set(ItemStack itemStack) {
+
+            // If the value is null then set it or if it's a T then set it
+            if (itemStack == null) ItemStack = null;
+            else if (itemStack is T otherItemStack) ItemStack = otherItemStack;
+
+        }
         public sealed override uint GetMaxStackSize(ItemStack itemStack)
             => itemStack == null ? GetMaxStackSize(null) : itemStack is T otherItemStack ? GetMaxStackSize(otherItemStack) : 0;
 
         public sealed override void Give(ItemStack itemStack) {
-            if (itemStack is T)
-                base.Give(itemStack);
+            if (itemStack is T otherItemStack)
+                Give(otherItemStack);
         }
         public sealed override void Take(ItemStack itemStack) {
-            if (itemStack is T)
-                base.Take(itemStack);
+            if (itemStack is T otherItemStack)
+                Take(otherItemStack);
         }
 
         public sealed override bool CanBeGiven(ItemStack itemStack)
-            => itemStack == null ? CanBeGiven(null) : itemStack is T otherItemStack ? CanBeGiven(otherItemStack) : false;
+            => itemStack == null ? CanBeGiven(null) : itemStack is T otherItemStack && CanBeGiven(otherItemStack);
         public sealed override bool CanBeTaken(ItemStack itemStack)
-            => itemStack == null ? CanBeTaken(null) : itemStack is T otherItemStack ? CanBeTaken(otherItemStack) : false;
+            => itemStack == null ? CanBeTaken(null) : itemStack is T otherItemStack && CanBeTaken(otherItemStack);
 
     }
 
@@ -67,26 +75,31 @@ namespace Sacados {
 
         public virtual uint GetMaxStackSize(ItemStack itemStack) => maxStackSize == 0 && !itemStack.IsEmpty() ? itemStack.Item.MaxStackSize : maxStackSize;
 
-        public virtual ItemStack ItemStack {
-            get => container[Index]; set {
+        // Can't override this and have at the same time new T ItemStack
+        // => To go around this issue, just use two methods for the property
+        public virtual ItemStack ItemStack { get => Get(); set => Set(value); }
 
-                // If the ItemStack can't be set
-                if (!CanBeGiven(value)) return;
+        /// <inheritdoc cref="ItemStack"/>
+        protected virtual ItemStack Get() => container[Index];
+        /// <inheritdoc cref="ItemStack"/>
+        protected virtual void Set(ItemStack itemStack) {
 
-                // If the ItemStack is empty then just set it
-                if (value.IsEmpty()) container[Index] = null;
-                else {
+            // If the ItemStack can't be set
+            if (!CanBeGiven(itemStack)) return;
 
-                    // Transfer the maximum stack size (don't forget to clone the original stack)
-                    uint transfer = Math.Min(GetMaxStackSize(value), value.StackSize);
-                    ItemStack stack = value.Clone();
-                    stack.StackSize = transfer;
-                    value.StackSize -= transfer;
-                    container[Index] = stack;
+            // If the ItemStack is empty then just set it
+            if (itemStack.IsEmpty()) container[Index] = null;
+            else {
 
-                }
+                // Transfer the maximum stack size (don't forget to clone the original stack)
+                uint transfer = Math.Min(GetMaxStackSize(itemStack), itemStack.StackSize);
+                ItemStack stack = itemStack.Clone();
+                stack.StackSize = transfer;
+                itemStack.StackSize -= transfer;
+                container[Index] = stack;
 
             }
+
         }
 
         // By default accept to give and take any ItemStack
@@ -138,7 +151,6 @@ namespace Sacados {
         // Mark the slot as dirty by just setting back it's itemstack to the same value
         // TODO: Maybe put this in the ISlot interface so that we can update it from outside?
         protected void MarkDirty() => container[Index] = ItemStack;
-
 
     }
 
