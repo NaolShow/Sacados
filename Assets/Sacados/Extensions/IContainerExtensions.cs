8a224169d3a5e119af4cd524f8d990a6e34a2c16
NaolShow <loan.jouhannet@protonmail.com>
2023-06-17 07:48:52 +0200

    feat(extensions): remove useless methods and fix filters

--- Assets/Sacados/Extensions/IContainerExtensions.cs
+++ Assets/Sacados/Extensions/IContainerExtensions.cs
@@ -6,50 +6,36 @@
     public static class IContainerExtensions {
 
         /// <summary>
-        /// Determines the total number of <see cref="ItemStack"/> that could be taken from the <see cref="IContainer{T}"/>
+        /// Determines the total number of <see cref="ItemStack"/> that could be taken from the <see cref="IContainer"/>
         /// </summary>
         /// <param name="container">The <see cref="IContainer"/> that contains the <see cref="ItemStack"/></param>
         /// <param name="itemStack">The <see cref="ItemStack"/> that will be counted</param>
-        /// <returns>The total number of <see cref="ItemStack"/> that is in the <see cref="IContainer{T}"/></returns>
+        /// <returns>The total number of <see cref="ItemStack"/> that is in the <see cref="IContainer"/></returns>
         public static ulong GetCount(this IContainer container, ItemStack itemStack) {
 
             ulong count = 0;
-            for (int i = 0; i < container.SlotsCount; i++)
-                count += container.Get(i).GetCount(itemStack);
+            if (container.CanBeTaken(itemStack))
+                for (int i = 0; i < container.SlotsCount; i++)
+                    count += container.Get(i).GetCount(itemStack);
             return count;
 
         }
 
         /// <summary>
-        /// Determines the remaining space for <see cref="ItemStack"/> that could be given to the <see cref="IContainer{T}"/>
+        /// Determines the remaining space for <see cref="ItemStack"/> that could be given to the <see cref="IContainer"/>
         /// </summary>
         /// <param name="container">The <see cref="IContainer"/> that contains the <see cref="ItemStack"/></param>
-        /// <param name="itemStack">The <see cref="ItemStack"/> that will get it's remaining space counted</param>
-        /// <returns>The remaining space for <see cref="ItemStack"/> in the <see cref="IContainer{T}"/></returns>
+        /// <param name="itemStack">The <see cref="ItemStack"/> used to calculate the remaining space</param>
+        /// <returns>The remaining space for <see cref="ItemStack"/> in the <see cref="IContainer"/></returns>
         public static ulong GetSpace(this IContainer container, ItemStack itemStack) {
 
             ulong count = 0;
-            for (int i = 0; i < container.SlotsCount; i++)
-                count += container.Get(i).GetSpace(itemStack);
+            if (container.CanBeGiven(itemStack))
+                for (int i = 0; i < container.SlotsCount; i++)
+                    count += container.Get(i).GetSpace(itemStack);
             return count;
 
         }
-
-        /// <summary>
-        /// Determines if the specified <see cref="ItemStack"/> can be given to the <see cref="IStackContainer{T}"/>
-        /// </summary>
-        /// <param name="container">The <see cref="IStackContainer"/> to which the <see cref="ItemStack"/> might be given</param>
-        /// <param name="itemStack">The <see cref="ItemStack"/> that could be given to the <see cref="IStackContainer{T}"/></param>
-        /// <returns>True if there is enough space for the <see cref="ItemStack"/> in the <see cref="IStackContainer{T}"/> to be given, false otherwise</returns>
-        public static bool HasEnoughSpace<T>(this IContainer<T> container, T itemStack) where T : ItemStack => container.GetSpace(itemStack) >= itemStack.StackSize;
-
-        /// <summary>
-        /// Determines if the specified <see cref="ItemStack"/> can be taken from the <see cref="IStackContainer{T}"/>
-        /// </summary>
-        /// <param name="container">The <see cref="IStackContainer"/> from which the <see cref="ItemStack"/> might be taken</param>
-        /// <param name="itemStack">The <see cref="ItemStack"/> that could be taken from the <see cref="IStackContainer{T}"/></param>
-        /// <returns>True if there is enough <see cref="ItemStack"/> in the <see cref="IStackContainer{T}"/> to be taken, false otherwise</returns>
-        public static bool HasEnoughCount<T>(this IContainer<T> container, T itemStack) where T : ItemStack => container.GetCount(itemStack) >= itemStack.StackSize;
 
     }
 
--- Assets/Sacados/Extensions/ISlotExtensions.cs
+++ Assets/Sacados/Extensions/ISlotExtensions.cs
@@ -16,10 +16,13 @@
             uint count = 0;
 
             // If the slot's ItemStack is not empty and are the same then set the StackSize
-            ItemStack slotItemStack = slot.ItemStack;
-            if (!slotItemStack.IsEmpty() && slotItemStack.IsSameAs(itemStack))
-                count = slotItemStack.StackSize;
+            if (slot.CanBeTaken(itemStack)) {
 
+                ItemStack slotItemStack = slot.ItemStack;
+                if (!slotItemStack.IsEmpty() && slotItemStack.IsSameAs(itemStack))
+                    count = slotItemStack.StackSize;
+
+            }
             return count;
 
         }
@@ -28,7 +31,7 @@
         /// Determines the remaining space for <see cref="ItemStack"/> that could be given to the <see cref="ISlot"/>
         /// </summary>
         /// <param name="slot">The <see cref="ISlot"/> that contains the <see cref="ItemStack"/></param>
-        /// <param name="itemStack">The <see cref="ItemStack"/> that will get it's remaining space counted</param>
+        /// <param name="itemStack">The <see cref="ItemStack"/> used to calculate the remaining space</param>
         /// <returns>The remaining space for <see cref="ItemStack"/> in the <see cref="ISlot"/></returns>
         public static uint GetSpace(this ISlot slot, ItemStack itemStack) {
 
@@ -51,27 +54,11 @@
         }
 
         /// <summary>
-        /// Determines if the <see cref="ISlot"/> has enough space to be given the <see cref="ItemStack"/>
-        /// </summary>
-        /// <param name="slot">The <see cref="ISlot"/> to which the <see cref="ItemStack"/> might be given</param>
-        /// <param name="itemStack">The <see cref="ItemStack"/> that could be given to the <see cref="ISlot"/></param>
-        /// <returns>True if there is enough space for the <see cref="ItemStack"/> in the <see cref="ISlot"/> to be given, false otherwise</returns>
-        public static bool HasEnoughSpace(this ISlot slot, ItemStack itemStack) => slot.GetSpace(itemStack) >= itemStack.StackSize;
-
-        /// <summary>
-        /// Determines if the <see cref="ISlot"/> has enough <see cref="ItemStack"/> to be taken
-        /// </summary>
-        /// <param name="slot">The <see cref="ISlot"/> from which the <see cref="ItemStack"/> might be taken</param>
-        /// <param name="itemStack">The <see cref="ItemStack"/> that could be taken from the <see cref="ISlot"/></param>
-        /// <returns>True if there is enough <see cref="ItemStack"/> in the <see cref="ISlot"/> to be taken, false otherwise</returns>
-        public static bool HasEnoughCount(this ISlot slot, ItemStack itemStack) => slot.GetCount(itemStack) >= itemStack.StackSize;
-
-        /// <summary>
         /// Swaps <see cref="ItemStack"/> of the two specified <see cref="ISlot"/>
         /// </summary>
         /// <param name="slot">The first <see cref="ISlot"/></param>
         /// <param name="otherSlot">The second <see cref="ISlot"/></param>
-        /// <returns>True if both <see cref="ISlot"/> could have their <see cref="ItemStack"/> taken and are both compatible with them, otherwise false</returns>
+        /// <returns>True if both <see cref="ISlot"/> could have their <see cref="ItemStack"/> taken and given, otherwise false</returns>
         public static bool Swap(this ISlot slot, ISlot otherSlot) {
 
             ItemStack itemStack = slot.ItemStack;
